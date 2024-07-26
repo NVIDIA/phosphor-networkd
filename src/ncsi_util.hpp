47b56413ebcbd8634584be78f906195a23a1f381
Jeremy Kerr <jk@codeconstruct.com.au>
2024-07-26 17:19:57 +0800

    ncsi: log interface types using to_string
    Add a to_string() method for Interfaces, to handle printing via lg2.
    
    Since we're printing the interface object, don't specify _INDEX on the
    metadata, this may not always be an index.
    
    Also, interface indices are typically referred to as decimal. Use
    decimal instead of a lg2::hex conversion.
    
    Change-Id: I90b5fbac52adde3bbc3f2699087373ed96361083
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- src/ncsi_util.hpp
+++ src/ncsi_util.hpp
@@ -5,7 +5,10 @@
 #include <optional>
 #include <span>
 #include <string>
+<<<<<<< HEAD
 #include <vector>
+=======
+>>>>>>> 8a76d89 (ncsi: log interface types using to_string)
 
 namespace phosphor
 {
@@ -97,6 +100,24 @@ struct Interface
 };
 
 std::string to_string(Interface& interface);
+<<<<<<< HEAD
+=======
+
+/* @brief  This function will ask underlying NCSI driver
+ *         to send an OEM command (command type 0x50) with
+ *         the specified payload as the OEM data.
+ *         This function talks with the NCSI driver over
+ *         netlink messages.
+ * @param[in] interface - Interface
+ * @param[in] package - NCSI Package.
+ * @param[in] channel - Channel number with in the package.
+ * @param[in] opcode  - NCSI Send Command sub-operation
+ * @param[in] payload - OEM data to send.
+ * @returns 0 on success and negative value for failure.
+ */
+int sendOemCommand(Interface& interface, int package, int channel, int opcode,
+                   std::span<const unsigned char> payload);
+>>>>>>> 8a76d89 (ncsi: log interface types using to_string)
 
 struct NetlinkInterface : Interface
 {
